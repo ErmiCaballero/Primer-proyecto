9cc54bc9d86991b0ef50f091be160e7e34cd4ab7
ErmiCaballero <ermicaballero@gmail.com>
2018-03-27 11:36:24 -0600

    Añadir todo

---
+++ hola.cpp
@@ -0,0 +1,27 @@
+/*	Parte 1
+*
+*	Agregar un Saludo con su nombre en la parte correspondiente
+*	ej. Aldea te saluda Oskar
+*
+*	<==========================================================>
+*	Parte 2
+*
+*	Descomentar el agradecimiento al curso
+*/
+
+#include <iostream>
+
+
+int main()
+{
+	puts("\t\tBienvenidos a Aldea UPP 2018");
+	puts("\n\tTaller de GitHub Basico");
+	
+//	puts("\n\n\tAldea te saluda [Nombre]);	SALUDO
+//	puts("\n\tGracias por asistir!!");		AGRADECIMIENTO
+	
+	puts("\n\n\n\t\t\t\t\t\t\t\tHecho por Oskar -- Fundador de DevAimKeys (DAK)");
+	
+	system("PAUSE");
+}
+
